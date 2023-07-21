069fe098bcb215e162b8597fed6a13d9e9892b67
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-21 15:51:05 +0300

    2. Use the require function

--- Chapter 2/02_02/global.js
+++ Chapter 2/02_02/global.js
@@ -1,3 +1,9 @@
-let hello = "Hello World from Node.js";
-let justNode = hello.slice(17);
-console.log(`Who let the ${justNode} out?`);
+const path = require('path');
+
+console.log(__dirname);
+console.log(__filename);
+console.log(`The file name is ${path.basename(__filename)}`);
+
+for (let key in global){
+    console.log(key);
+}
