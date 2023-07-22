bc91f426dc71a87646407c2387ae22f7142566ec
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2023-07-22 18:29:39 +0300

    7. Consume a module with EventEmitter

--- Chapter 3/03_07/ask.js
+++ Chapter 3/03_07/ask.js
@@ -1,20 +1,15 @@
-const collectAnswers = require("./lib/collectAnswers");
+const collectAnswers = require('./lib/collectAnswers');
 
 const questions = [
-  "What is your name?",
-  "Where do you live?",
-  "What are you going to do with Node.js?"
+  "What is your name? ",
+  "Where do you live? ",
+  "What are you going to do with Node.js? ",
 ];
 
-const answerEvents = collectAnswers(
-  questions,
-  (answers) => {
-    console.log("Thank you for your answers!");
-    console.log(answers);
-    process.exit();
-  }
-);
+const answerEvents =  collectAnswers(questions, (answers) => {
+  console.log('Thank you for your answers:');
+  console.log(answers);
+  process.exit();
+});
 
-answerEvents.on("answer", (answer) =>
-  console.log(`The answer is ${answer}`)
-);
+answerEvents.on('answer', (answer) => console.log(`The answer is ${answer}`));
--- Chapter 3/03_07/lib/collectAnswers.js
+++ Chapter 3/03_07/lib/collectAnswers.js
@@ -1,30 +1,27 @@
 const readline = require("readline");
-const { EventEmitter } = require("events");
+const {EventEmitter} = require('events');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout,
 });
 
-module.exports = (questions, done) => {
-  const answers = [];
-  const [firstQuestion] = questions;
-
-  const emitter = new EventEmitter();
-
-  const questionAnswered = (answer) => {
-    emitter.emit("answer", answer);
-    answers.push(answer.trim());
-    if (answers.length < questions.length) {
-      rl.question(
-        questions[answers.length],
-        questionAnswered
-      );
-    } else {
-      return done(answers);
-    }
-  };
-
-  rl.question(firstQuestion, questionAnswered);
-  return emitter;
+module.exports =  (questions, done) => {
+    const answers = [];
+    const [firstQuestion] = questions;
+    const emitter = new EventEmitter();
+    
+    const answerQuestion = (answer) => {
+      emitter.emit('answer', answer);
+      answers.push(answer.trim());
+      
+      if(answers.length < questions.length){
+        rl.question(questions[answers.length], answerQuestion);
+      } else {
+        return done(answers);
+      }
+    };
+    
+    rl.question(firstQuestion, answerQuestion);
+    return emitter;
 };
